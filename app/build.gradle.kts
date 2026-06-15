3a090cc6a545fdf321c9d8b768776a1cadaa4f49
diademma <77641647+diademma@users.noreply.github.com>
2026-06-15 17:45:45 +0300

    bump: update versionName to 1.1 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = (System.getenv("GITHUB_RUN_NUMBER")?.toIntOrNull() ?: 1)
-    versionName = "1.0"
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
